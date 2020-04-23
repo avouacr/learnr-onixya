53b36a1f46af61482015d30fce349b2065d898ad
Romain Lesur <romain.lesur@gmail.com>
2020-04-23 14:59:21 +0200

    add onyxia init script

---
+++ onyxia.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+REPO_URL=https://github.com/rlesur/learnr-onixya.git
+
+# Clone the repository in /home/rstudio/tutoriel
+mkdir /home/rstudio/tutoriel
+git clone $REPO_URL /home/rstudio/tutoriel
+
+# Open the project
+echo \
+"
+setHook('rstudio.sessionInit', function(newSession) {
+  if (newSession && identical(getwd(), path.expand('~')))
+  {
+    message('Ouverture du tutoriel')
+    rstudioapi::openProject('~/tutoriel')
+  }
+}, action = 'append')
+" >> /home/rstudio/.Rprofile
