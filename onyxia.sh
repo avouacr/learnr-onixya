5c6530a00b843ebb09b954c414715c9ee995f65c
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2023-06-29 11:43:33 +0200

    Update onyxia.sh

--- onyxia.sh
+++ onyxia.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-REPO_URL=https://github.com/RLesur/learnr-onixya.git
+REPO_URL=https://github.com/avouacr/learnr-onixya.git
+TUTO_DIR=/home/onyxia/work/tutoriel
 
 # Clone the repository in /home/rstudio/tutoriel
-mkdir /home/rstudio/tutoriel
-git clone $REPO_URL /home/rstudio/tutoriel
+git clone $REPO_URL $TUTO_DIR
 
 # Install dependencies
-Rscript -e "remotes::install_deps(pkgdir = '/home/rstudio/tutoriel', dependencies = TRUE, upgrade = FALSE)"
+Rscript -e "remotes::install_deps(pkgdir = ${TUTO_DIR}, dependencies = TRUE, upgrade = FALSE)"
 
 # Open the project
 echo \
@@ -16,7 +16,7 @@ setHook('rstudio.sessionInit', function(newSession) {
   if (newSession && identical(getwd(), path.expand('~')))
   {
     message('Ouverture du tutoriel')
-    rstudioapi::openProject('~/tutoriel')
+    rstudioapi::openProject('~/work/tutoriel')
   }
 }, action = 'append')
 " >> /home/rstudio/.Rprofile
