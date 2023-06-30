4602fff747c5ba7f99c7583662e455668a8612f4
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2023-06-30 09:58:34 +0200

    Update onyxia.sh

--- onyxia.sh
+++ onyxia.sh
@@ -6,10 +6,11 @@ TUTO_DIR=${WORK_DIR}/tutoriel
 
 # Clone the repository in /home/rstudio/tutoriel
 git clone $REPO_URL $TUTO_DIR
+mc cp s3/avouacr/sect01_seq_01(1).qmd $TUTO_DIR/demo/sect01_seq_01(1).qmd
 chown -R onyxia:users $TUTO_DIR
 
 # Install dependencies
-Rscript -e "remotes::install_deps(pkgdir = '${TUTO_DIR}', dependencies = TRUE, upgrade = FALSE)"
+# Rscript -e "remotes::install_deps(pkgdir = '${TUTO_DIR}', dependencies = TRUE, upgrade = FALSE)"
 
 # Open the project
 echo \
